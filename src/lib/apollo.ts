7c93d62f4136d1fe84f3d8abc44ec6cb344546ce
Guilherme Ferrari <guiferrari10@hotmail.com>
2022-06-27 14:15:39 -0300

    feat(subscribe): Created subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pmykot2b1901w7htmtdd79/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
